73db89b5984cf8227064491aa032350d4324ff6e
heroyf <y13788978852@gmail.com>
2018-09-25 11:08:55 +0800

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+ 
 class Solution:
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,21 +11,18 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        location = ListNode(0)
-        result = location
-        sum=0
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-        while l1 or l2:
-            if l1:
+        temp = ListNode(0)
+        result = temp
+        sum = 0
+        while l1 != None or l2 !=None or sum != 0:
+            if l1 != None:
                 sum += l1.val
-                l1=l1.next
-            if l2:
+                l1 = l1.next
+            if l2 != None:
                 sum += l2.val
-                l2=l2.next
-            location.next = ListNode(sum%10)
-            location = location.next
-            sum = int(sum //10)
+                l2 = l2.next
+            temp.next = ListNode(a%10)
+            temp = temp.next
+            sum=int(sum//10)
         return result.next
+
